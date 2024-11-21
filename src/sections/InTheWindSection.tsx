803bb2a36d09fbefe464d6bce34806a03752f25a
Shinn Thant <94442302+DevShinnThant@users.noreply.github.com>
2024-11-21 11:56:20 +0630

    feat: implement in-to-the-wind section ui

--- src/sections/InTheWindSection.tsx
+++ src/sections/InTheWindSection.tsx
@@ -1,62 +1,125 @@
+import { MouseEventHandler, useRef } from "react"
+import gsap from "gsap"
+
 export default function InTheWindSection() {
+  const clipRef = useRef(null)
+
+  const handleMouseEnter: MouseEventHandler<HTMLDivElement> = (event) => {
+    if (!event.currentTarget) return
+    const rect = (event.currentTarget as HTMLElement).getBoundingClientRect()
+    const x = ((event.clientX - rect.left) / rect.width) * 100
+    const y = ((event.clientY - rect.top) / rect.height) * 100
+
+    gsap.to(clipRef.current, {
+      clipPath: `circle(141.4% at ${x}% ${y}%)`,
+      duration: 0.4,
+      ease: "sine",
+    })
+  }
+
+  const handleMouseLeave: MouseEventHandler<HTMLDivElement> = (event) => {
+    if (!event.currentTarget) return
+    const rect = (event.currentTarget as HTMLElement).getBoundingClientRect()
+    const x = ((event.clientX - rect.left) / rect.width) * 100
+    const y = ((event.clientY - rect.top) / rect.height) * 100
+
+    gsap.to(clipRef.current, {
+      clipPath: `circle(0% at ${x}% ${y}%)`,
+      duration: 0.3,
+
+      ease: "sine",
+    })
+  }
+
   return (
     <section
       id="space-section"
-      className="relative z-10 h-[200vh] w-screen bg-orange-500 text-black"
+      className="relative z-10 h-screen w-screen bg-black"
     >
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores
-      temporibus reprehenderit qui quidem, eum distinctio cumque consequuntur
-      dolore neque aperiam, quasi nihil ab magni a sint, excepturi explicabo hic
-      optio ad nostrum illum! Amet quos cumque ut qui sint laudantium sapiente,
-      necessitatibus rem nemo ad ratione. Fuga, id omnis! Reprehenderit beatae
-      suscipit, magnam ratione natus est esse voluptatum officia animi inventore
-      consequatur corporis illo eius pariatur praesentium itaque quidem nesciunt
-      dolore optio, incidunt cupiditate deserunt deleniti corrupti ad! Tempore,
-      magnam eum, consectetur est ratione laudantium pariatur doloremque
-      exercitationem quibusdam minus sint dignissimos perspiciatis amet impedit,
-      aliquid sed non fugiat quia soluta aperiam deleniti eos. Corrupti,
-      voluptatum sed. Quos reiciendis iure error. Enim natus officiis harum amet
-      dolorum ipsam animi sapiente placeat deleniti possimus aut soluta
-      provident consequuntur aliquid cumque consectetur eum, minima quibusdam
-      cum itaque tempora iure. Consequuntur illum inventore delectus similique
-      quaerat qui pariatur voluptas neque fuga sequi quidem sunt sit, beatae
-      dolore dicta possimus quis voluptatibus animi debitis molestias ea placeat
-      magnam suscipit. Nesciunt reiciendis deserunt hic accusantium temporibus
-      vitae. Laudantium natus, suscipit sint sapiente assumenda autem beatae
-      facilis nobis, aut aliquid nisi nihil! Numquam ab possimus autem maiores
-      tenetur quaerat voluptas aut necessitatibus dolore, odit cumque dolor
-      laborum! Asperiores pariatur veritatis corrupti, laboriosam odit unde
-      blanditiis voluptas ad, fugit incidunt aliquam nulla, cupiditate odio
-      cumque beatae error omnis. Impedit debitis nemo dicta quod saepe
-      dignissimos natus. Natus non atque cupiditate dolores. Fuga similique
-      aperiam quas in, repellat nisi perferendis. Magni expedita ex eligendi ad
-      cum ullam deleniti possimus, repellendus hic quo architecto, odit, et
-      voluptatibus ratione temporibus culpa maiores explicabo? Qui nihil
-      voluptate eaque, quibusdam blanditiis eligendi velit placeat ipsa dolore
-      id praesentium tempora! Dolorem ut ullam tempora accusantium, maiores quis
-      fuga assumenda, explicabo temporibus provident blanditiis eaque modi eius
-      illum nemo dolorum asperiores facere perspiciatis quidem ducimus commodi
-      nihil a? Eligendi laboriosam ex odit magnam voluptatibus unde eius eveniet
-      qui ea nesciunt! Pariatur perspiciatis voluptas dolor laboriosam fugiat
-      tempore repellat. Hic commodi, itaque temporibus iusto, obcaecati quae
-      aliquid, suscipit dignissimos eum in aliquam atque tempore expedita sequi
-      alias adipisci odit neque modi nemo? Quis doloribus et cupiditate repellat
-      dolore maiores, modi necessitatibus nam blanditiis eaque neque. Aspernatur
-      quidem quae hic? Voluptates eligendi quas totam repudiandae itaque magnam
-      illum, distinctio eveniet iusto natus aut a ut nemo dolore recusandae
-      tempora assumenda numquam. Nesciunt maiores, deserunt velit deleniti
-      expedita cupiditate nihil consequuntur nisi illum doloremque quaerat
-      rerum, natus ipsam adipisci. Maxime quo sunt nihil. Tenetur in,
-      exercitationem, qui laudantium expedita iusto nihil mollitia assumenda
-      modi quos voluptatum tempora ea ipsam tempore recusandae nulla quam totam
-      voluptates sapiente quisquam eum facilis quidem corrupti cupiditate? Dicta
-      doloremque odit cumque repellendus sed eveniet labore, corporis eum qui
-      consequuntur placeat tempora quia ex est impedit sint earum! Ipsa hic
-      beatae, similique quod nemo vel illo dolorum minima debitis iure, tenetur
-      neque consectetur dolores deserunt nam quaerat optio, saepe voluptate
-      nobis inventore porro minus dignissimos. Voluptatum ullam sint alias quas,
-      maxime distinctio maiores voluptatem, ab, nostrum non illum numquam nobis.
-      Veniam, consequatur rerum?
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="flex flex-col items-center gap-5">
+          <div
+            style={
+              {
+                "-webkit-mask-image":
+                  "linear-gradient(#0000 0%,#fff 0%,#0000 85% 100%)",
+              } as React.CSSProperties
+            }
+            className="font-heading text-[22cqw] leading-[1] text-white"
+          >
+            In the wild
+          </div>
+          <div className="w-[35%] text-center text-[.75rem] leading-[1.4em] tracking-[0.1em] text-white">
+            ARE YOU RELAXING ON VACATION AND CAUGHT SIGHT OF A SPRINTER VAN?
+            EATING DINNER AND THE GALS WALTZ IN, ONLY TO THROW SALAD, WINE
+            GLASSES, OR NAPKINS AT ONE ANOTHER? SEND US THE RECEIPTS, PROOF,
+            TIMELINES, SCREENSHOTS. SUBMISSIONS WILL BE FEATURED ON OUR SITE AND
+            SOCIAL MEDIA.
+          </div>
+
+          <div
+            className="group relative flex flex-col items-center justify-center overflow-hidden rounded-full border transition-all hover:border-white"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            {/* Button Content */}
+            <div className="flex h-[2.5rem] w-full bg-white px-[1rem] leading-[1] text-black">
+              <div className="mx-auto flex items-center justify-center gap-1">
+                <div className="relative z-[2] text-[.75rem] uppercase leading-[1.4em] group-hover:text-white">
+                  Discover the wild ones
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="relative z-[2] order-[-1] scale-[0.6] group-hover:stroke-white"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M8 4h-2l-3 10v2.5" />
+                  <path d="M16 4h2l3 10v2.5" />
+                  <path d="M10 16l4 0" />
+                  <path d="M17.5 16.5m-3.5 0a3.5 3.5 0 1 0 7 0a3.5 3.5 0 1 0 -7 0" />
+                  <path d="M6.5 16.5m-3.5 0a3.5 3.5 0 1 0 7 0a3.5 3.5 0 1 0 -7 0" />
+                </svg>
+              </div>
+            </div>
+
+            <div
+              ref={clipRef}
+              className="pointer-events-none absolute inset-0 h-[2.5rem] w-full rounded-[100vw] bg-black"
+              style={{ clipPath: "circle(0% at 50% 50%)" }}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Images */}
+      <div className="absolute left-[34%] top-[2%]">
+        <img
+          className="h-[15vh] w-[7vw]"
+          src="https://cdn.prod.website-files.com/66bdbd95953ed41b630aa4ba/66c658c0fc8c1bc4501bcb52_sunset.avif"
+        />
+      </div>
+
+      <div className="absolute right-[22%] top-[17%]">
+        <img
+          className="h-[10vh] w-[8vw]"
+          src="https://cdn.prod.website-files.com/66bdbd95953ed41b630aa4ba/66c6587ddf0ae8fd5c7d663f_12_Medley-Watson.avif"
+        />
+      </div>
+
+      <div className="absolute bottom-[40%] left-[18%]">
+        <img
+          className="h-[11vh] w-[9vw]"
+          src="https://cdn.prod.website-files.com/66bdbd95953ed41b630aa4ba/66c658b3053670fd133cf579_RHOC_ITW_Surf%20%26%20Sand%20Resort%20Laguna%20Beach_-topaz-upscale-2500w.avif"
+        />
+      </div>
     </section>
   )
 }
